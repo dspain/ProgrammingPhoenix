25a95c0bf0001d853f5a5abf957498cd1fe073de
dspain <dmspain@yahoo.com>
2017-11-07 22:33:04 -0800

    fix bcrypt

--- rumbl/mix.exs
+++ rumbl/mix.exs
@@ -38,7 +38,8 @@ defmodule Rumbl.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 4.0"}]
+     {:comeonin, "~> 4.0"},
+     {:bcrypt_elixir, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
