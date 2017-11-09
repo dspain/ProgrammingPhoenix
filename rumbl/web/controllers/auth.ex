2953897827871e7e455b087c3bcda882a9d1b478
dspain <dmspain@yahoo.com>
2017-11-08 21:40:47 -0800

    typo

--- rumbl/web/controllers/auth.ex
+++ rumbl/web/controllers/auth.ex
@@ -15,6 +15,6 @@ defmodule Rumbl.Auth do
     conn
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
-    |> configure_sessioin(renew: true)
+    |> configure_session(renew: true)
   end
 end
