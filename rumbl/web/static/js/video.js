80677888566ac2c01b2d49302a03670598abefc6
dspain <dmspain@yahoo.com>
2017-11-17 21:24:09 -0800

    join channel from the client

--- rumbl/web/static/js/video.js
+++ rumbl/web/static/js/video.js
@@ -13,8 +13,12 @@ let Video = {
   onReady(videoId, socket) {
     let msgContainer = document.getElementById("msg-container")
     let msgInput     = document.getElementById("msg-input")
+    let postButton   = document.getElementById("msg-submit")
     let vidChannel   = socket.channel("videos:" + videoId)
-    // TODO join the vidChannel
+
+    vidChannel.join()
+      .receive("ok",    resp   => console.log("joined the video channel", resp)   )
+      .receive("error", reason => console.log("join failed",              reason) )
   }
 }
 export default Video
